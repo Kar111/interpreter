7fb54a23a984d6acba2f8288b58fc69d83b4253d
Kar111 <avetisyankarine111@gmail.com>
2023-04-17 12:15:34 +0400

    git add Main.java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+
+package org.example;
+
+import javafx.application.Application;
+import org.example.editor.EditorApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(EditorApplication.class, args);
+    }
+}
